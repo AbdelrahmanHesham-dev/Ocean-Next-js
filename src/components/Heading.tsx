d10415776a054a0967c7165b201adf265d4d2e87
Abdelrahman <abdelrahmanhesham477@gmail.com>
2024-10-16 17:40:00 +0300

    ubdate

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -10,7 +10,7 @@ const Heading: FC<HeadingProps> = ({ title, isCentered }) => {
     <h1
       className={`${
         isCentered && "text-center"
-      } bg-clip-text text-transparent bg-gradient-to-tr from-sky-200 via-sky-500 to-sky-200 font-extrabold text-3xl sm:text-4xl lg:text-5xl lg:leading-5	`}
+      } bg-clip-text text-transparent bg-gradient-to-tr from-sky-200 via-sky-500 to-sky-200 font-extrabold text-3xl sm:text-4xl lg:text-5xl lg:leading-8`}
     >
       {title}
     </h1>
